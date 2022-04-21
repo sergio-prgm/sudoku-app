535fed537f7100d7c7eb8a39af6e39c53e4f6467
Sergio Dev <sergio-dev@outlook.com>
2022-04-21 10:23:44 +0200

    Feat: add delete button

--- src/components/Controls/controls.jsx
+++ src/components/Controls/controls.jsx
@@ -2,7 +2,7 @@ import Key from '../Key/Key'
 import './styles.scss'
 
 export default function Controls () {
-  const keys = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
   return (
     <div className="control-pad">
       {keys.map(key => {
