0d26f83670e976efb539fadabf30ade77919a626
sergio-prgm <sergio-prgm@outlook.com>
2022-04-29 20:24:37 +0200

    Refactor: set @ absolute paths

--- src/pages/SudokuPage/SudokuPage.jsx
+++ src/pages/SudokuPage/SudokuPage.jsx
@@ -1,7 +1,7 @@
 import './SudokuPage.scss'
-import Controls from '../../components/Controls/Controls'
-import SudokuBoard from '../../components/SudokuBoard/SudokuBoard'
-import Timer from '../../components/Timer/Timer'
+import Controls from '@/components/Controls/Controls'
+import SudokuBoard from '@/components/SudokuBoard/SudokuBoard'
+import Timer from '@/components/Timer/Timer'
 
 export default function SudokuPage () {
   return (
