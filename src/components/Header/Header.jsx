b623ddb62de67e25c39ebe05e14717c4d1152ad3
sergio-dev <sergio-dev@outlook.com>
2022-05-31 18:35:08 +0200

    Feat: render login buttons conditionally

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,40 @@
-import { Link } from 'wouter'
+import { Link, useRoute } from 'wouter'
+import useUser from '@/hooks/useUser'
 
 export default function Header () {
+  const { isLogged, logout } = useUser()
+  // const [match] = useRoute('/login')
+  const { username } = JSON.parse(window.sessionStorage.getItem('jwt'))
+  const [, params] = useRoute('/:path')
+  console.log(params)
+
+  const handleClick = e => {
+    e.preventDefault()
+    logout()
+  }
+
+  const renderLogin = ({ isLogged }) => {
+    return isLogged
+      ? <><Link href='/' onClick={handleClick}>
+          Logout
+        </Link>
+        <p>{username}</p>
+        </>
+      : <><Link to='/login' >Login</Link>
+            <Link to='/register' >Register</Link>
+          </>
+  }
+
+  const toRender = params?.path === 'register' || params?.path === 'login'
+    ? null
+    : renderLogin({ isLogged })
+
   return (
     <header>
       <h1>
         <Link to='/'>Home</Link>
       </h1>
-      <Link to='/login' >Login</Link>
-      <br />
-      <Link to='/register' >Register</Link>
+      {toRender}
     </header>
   )
 }
