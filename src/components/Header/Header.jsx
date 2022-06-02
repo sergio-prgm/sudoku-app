c7d3812e4584f242221c11e24bdff97b404b5db2
sergio-dev <sergio-dev@outlook.com>
2022-06-02 18:54:45 +0200

    Fix: styles for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import { Link, useRoute } from 'wouter'
 import useUser from '@/hooks/useUser'
+import './styles.scss'
+import logo from '@/assets/icon/Sudoku_Logo.svg'
 
 export default function Header () {
   const { isLogged, logout } = useUser()
@@ -8,7 +10,6 @@ export default function Header () {
     ? JSON.parse(window.sessionStorage.getItem('jwt'))
     : null
   const [, params] = useRoute('/:path')
-  console.log(params)
 
   const handleClick = e => {
     e.preventDefault()
@@ -17,14 +18,14 @@ export default function Header () {
 
   const renderLogin = ({ isLogged }) => {
     return isLogged
-      ? <><Link href='/' onClick={handleClick}>
-          Logout
-        </Link>
-        <p>{storage.username}</p>
-        </>
-      : <><Link to='/login' >Login</Link>
-            <Link to='/register' >Register</Link>
-          </>
+      ? <div className='header--buttons'>
+          <Link href='/' onClick={handleClick} className='btn main'>Logout</Link>
+          <p className='btn alt'>{storage.username}</p>
+        </div>
+      : <div className='header--buttons'>
+          <Link to='/login' >Login</Link>
+          <Link to='/register' >Register</Link>
+        </div>
   }
 
   const toRender = params?.path === 'register' || params?.path === 'login'
@@ -32,10 +33,10 @@ export default function Header () {
     : renderLogin({ isLogged })
 
   return (
-    <header>
-      <h1>
-        <Link to='/'>Home</Link>
-      </h1>
+    <header className='header'>
+      <Link href='/' >
+        <img src={logo} className='header--logo' alt="logo img" height='55px' width='55px'/>
+      </Link>
       {toRender}
     </header>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,22 @@
+import { useContext, useEffect } from 'react'
 import Rules from '@/components/Rules/Rules'
 import Launcher from '@/components/Launcher/Launcher'
 import './Home.scss'
+import Context from '@/context/UserContext'
 
 export default function Home () {
+  const { savedSudokus } = useContext(Context)
+
+  useEffect(() => {
+    if (!savedSudokus[0]) return console.log('nothing saved (yet)')
+    console.log(savedSudokus)
+  }, [savedSudokus])
+
   return (
     <main>
       {/* <h1>Welcome to Sudoku-app</h1> */}
       <section className='launcher'>
-        <h2>play random sudoku</h2>
+        <h2>Play random sudoku</h2>
         <Launcher>Easy</Launcher>
         <Launcher>Medium</Launcher>
         <Launcher>Hard</Launcher>
@@ -17,6 +26,7 @@ export default function Home () {
       </section>
       <section className='collections'>
         <h2>Browse sudoku collection</h2>
+
       </section>
     </main>
   )
